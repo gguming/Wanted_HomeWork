756cfc0e97bcafc9cef6058046a2378cb18e061e
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2022-11-26 14:36:52 +0900

    command line tools 추가

---
+++ Wanted_Homework/Wanted_Homework/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Wanted_Homework
+//
+//  Created by SUN on 2022/11/26.
+//
+
+import Foundation
+
+private var input = ""
+while input != "X" {
+    input = readLine() ?? ""
+    print(input)
+}
